f372627d02c1cbc88d06dba9b886a98eeaa5a16c
alex <abdurashitob1999@mail.ru>
2019-10-26 16:11:24 +0300

    макет

---
+++ Assets/Script/CheckPoint.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public KeyCode rewindButton;
+    bool isRewind;
+    List<Vector3> positionList;
+    Rigidbody2D rb;
+
+
+    public float timeRewind;
+
+    void Start()
+    {
+        positionList = new List<Vector3>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRewind)
+        {
+            if (positionList.Count > 0)
+            {
+                int LastPosittion = positionList.Count - 1;
+                transform.position = Vector2.MoveTowards(positionList[LastPosittion], positionList[LastPosittion], 10 * Time.deltaTime);
+                // positionList[LastPosittion];
+                positionList.RemoveAt(LastPosittion);
+                rb.bodyType = RigidbodyType2D.Kinematic;
+            }
+        }
+        else
+        {
+            if (positionList.Count >= timeRewind * (1 / Time.deltaTime))
+                positionList.RemoveAt(0);
+
+            positionList.Add(transform.position);
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        if (Input.GetKeyDown(rewindButton))
+        {
+            isRewind = true;
+            
+        }
+        if (Input.GetKeyUp(rewindButton))
+        {
+            isRewind = false;
+        }
+    }
+}
---
+++ Assets/Script/Dead.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dead : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    public int Health=3;
+    public Text cointHelth;
+    private Color cr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cointHelth.text = Health.ToString();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health == 0)
+        {
+            cr = sr.color;
+            for (int i = 255;  i>= 0; i++)
+            {  
+                cr.a--;
+                sr.color = cr;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Respawn")
+        {
+            Health--;
+            cointHelth.text = Health.ToString();
+        }
+    }
+}
